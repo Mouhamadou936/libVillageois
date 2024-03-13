e168767d83f1e17a4e8fd8fc0c4cb1f1936abcfa
Mouhamadou <mouhamadou.lo.sio@gmail.com>
2024-03-13 11:00:16 +0100

    Ajoutez des fichiers projet.

---
+++ Villlage/Program.cs
@@ -0,0 +1,21 @@
+﻿using libVillageois;
+
+Village a, b;
+string rep;
+a = new Village(true, "Panoramix", 16);
+b = new Village("Abraracourcix");
+Console.WriteLine(b.getPoids());
+b.setPoids(26);
+Console.WriteLine(a.ToString());
+Console.WriteLine(a.getNom());
+Console.WriteLine(a.getPoids());
+Console.WriteLine(b.poidsSouleve()+"Kg");
+Console.WriteLine(a.soulevememepoids(b));
+if (a.poidsSouleve() > b.poidsSouleve())
+{
+    Console.WriteLine(a.getNom() + " soulève le plus de poids");
+}
+else
+{
+    Console.WriteLine(b.getNom() + " soulève le plus de poids");
+}
---
+++ libVillageois/Village.cs
@@ -0,0 +1,67 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace libVillageois
+{
+    public class Village
+    {
+        private bool _malade;
+
+        private string _nom;
+
+        private int _poids;
+        
+        public Village(bool malade,string nom, int poids)
+        {
+            this._malade = malade;
+            this._nom = nom;
+            this._poids = poids;
+        }
+        public Village(string nom)
+        {
+            this._nom = nom;
+            bool malade = false;
+        }
+
+        public override string ToString()
+        {
+            return "Le villageois se nomme "+this._nom+" peut soulever "+this._poids+" et est "+this._malade;
+        }
+        
+        public bool getMalade()
+        {
+            return _malade;
+        }
+        public string getNom()
+        {
+            return _nom;
+        }
+        public int getPoids()
+        {
+            return _poids;
+        }
+        public int setPoids(int poids)
+        { 
+            _poids = poids;
+            return poids; 
+        }
+
+        public int poidsSouleve()
+        {
+            int resultat = _poids / 5;
+
+            return resultat;
+        }
+        public bool soulevememepoids(Village a)
+        {
+            
+            if (this._poids == a.getPoids())
+            {
+                return true;
+            }
+            else return false; 
+            
+           
+        }
+
+    }
+}
